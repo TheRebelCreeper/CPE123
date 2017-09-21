dea665e5b66928b943ecf383af089b6055d6a600
TheRebelCreeper <therebelcreeper@gmail.com>
2017-09-21 09:22:21 -0700

    Initial Commit

---
+++ SkiGame/Player.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @Aaron Lampert, Tony Bone, Ritu Bhalodia, Elena Fowler, Jarod Morris
+ * @Beta 0.1
+ */
+public class Player extends Actor
+{
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
