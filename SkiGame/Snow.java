1848b66f2b2105b0b276b2c7cf0a264386978c79
elenarfowler <elenarfowler@gmail.com>
2017-10-05 09:40:28 -0700

    correct size slope

--- SkiGame/Snow.java
+++ SkiGame/Snow.java
@@ -12,9 +12,13 @@ public class Snow extends Actor
      * Act - do whatever the Snow wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Snow()
+    {
+        setRotation(10);
+    }
+    
     public void act() 
     {
         // Add your action code here.
-        setRotation(18);
     }    
 }
