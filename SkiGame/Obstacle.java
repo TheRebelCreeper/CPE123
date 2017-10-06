a6d0bd1259a3465b774b8b811c85b2145968bc60
TheRebelCreeper <therebelcreeper@gmail.com>
2017-10-06 13:07:42 -0700

    Changed paint order

--- SkiGame/Obstacle.java
+++ SkiGame/Obstacle.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Obstacles extends Actor
+public class Obstacle extends Actor
 {
     protected final int GRAVITY_ACCEL = 1;
     protected final int MAX_GRAV = 15;
