e1c3cb942b9b6b4ccef637eb37e5440a64306b2f
TheRebelCreeper <therebelcreeper@gmail.com>
2017-10-24 10:58:15 -0700

    Updated building interface

--- SkiGame/BuildWorld.java
+++ SkiGame/BuildWorld.java
@@ -36,6 +36,31 @@ public class BuildWorld extends World
         material1.setLocation(191,204);
         material1.setLocation(162,205);
         material1.setLocation(68,188);
+        removeObject(toolbar);
+        Buildable buildable = new Buildable();
+        addObject(buildable,84,79);
+        Buildable buildable2 = new Buildable();
+        addObject(buildable2,90,213);
+        Buildable buildable3 = new Buildable();
+        addObject(buildable3,97,344);
+        buildable3.setLocation(89,341);
+        buildable2.setLocation(84,207);
+        buildable3.setLocation(85,334);
+        removeObject(buildable2);
+        removeObject(buildable);
+        removeObject(buildable3);
+        material1.getImage();
+        Container container = new Container();
+        addObject(container,74,71);
+        Container container2 = new Container();
+        addObject(container2,82,206);
+        Container container3 = new Container();
+        addObject(container3,92,340);
+        container2.setLocation(74,204);
+        container3.setLocation(75,332);
+        Buildable buildable4 = new Buildable();
+        container.setObject(buildable4);
+        addObject(buildable4,74,79);
     }
     
 
