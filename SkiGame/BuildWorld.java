157609fca085c425d0c328cef1272505856ab774
TheRebelCreeper <therebelcreeper@gmail.com>
2017-10-25 12:50:40 -0700

    Added the remaining 2 buildables to build world

--- SkiGame/BuildWorld.java
+++ SkiGame/BuildWorld.java
@@ -25,25 +25,34 @@ public class BuildWorld extends World
     {   Snow snow = new Snow();
         addObject(snow,298,399);
 
-        Tree trees = new Tree();
-        addObject(trees,467,247);
-        trees.setLocation(489,252);
-        Material1 material1 = new Material1();
-        addObject(material1,73,204);
-        material1.setLocation(191,204);
-        material1.setLocation(162,205);
-        material1.setLocation(68,188);
-        material1.getImage();
+        Tree tree = new Tree();
+        addObject(tree,489,252);
+        
+        // Material1 material1 = new Material1();
+        // addObject(material1,73,204);
+        // material1.setLocation(191,204);
+        // material1.setLocation(162,205);
+        // material1.setLocation(68,188);
+
         Container container = new Container();
         addObject(container,74,71);
+
         Container container2 = new Container();
-        addObject(container2,82,206);
+        addObject(container2,74,204);
+
         Container container3 = new Container();
-        addObject(container3,92,340);
-        container2.setLocation(74,204);
-        container3.setLocation(75,332);
+        addObject(container3,74,332);
+
         Buildable buildable = new Buildable();
         container.setObject(buildable);
-        addObject(buildable,74,79);
+        addObject(buildable,74,71);
+
+        Buildable buildable2 = new Buildable();
+        container2.setObject(buildable2);
+        addObject(buildable2,74,203);
+
+        Buildable buildable3 = new Buildable();
+        container3.setObject(buildable3);
+        addObject(buildable3,74,333);
     }
 }
