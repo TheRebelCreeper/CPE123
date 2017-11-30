97fc9a3ec13eec22b64d9f977db11d28a7799622
tonytheonlypony <anthonybone@outlook.com>
2017-11-29 21:16:25 -0800

    Says space and shift instead of wasd

--- SkiGame/Rules.java
+++ SkiGame/Rules.java
@@ -17,7 +17,7 @@ public class Rules extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        setBackground(new GreenfootImage("Instructions.png"));
+        setBackground(new GreenfootImage("instructionsDos.png"));
     }
     
     public void act()
